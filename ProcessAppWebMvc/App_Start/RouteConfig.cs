18f100ad52e103c3017b4211e76126b35a561126
cristopher martinez <crisedderx@hotmail.com>
2020-10-11 13:04:45 -0300

    Se agrega la conexion a bd

--- ProcessAppWebMvc/App_Start/RouteConfig.cs
+++ ProcessAppWebMvc/App_Start/RouteConfig.cs
@@ -13,17 +13,17 @@ namespace ProcessAppWebMvc
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
-            //routes.MapRoute(
-            //    name: "Default",
-            //    url: "{controller}/{action}/{id}",
-            //    defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
-            //);
-
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Mantenedor", action = "Read", id = UrlParameter.Optional }
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
             );
+
+            //routes.MapRoute(
+            //    name: "listar",
+            //    url: "{controller}/{action}/{id}",
+            //    defaults: new { controller = "Mantenedor", action = "Read", id = UrlParameter.Optional }
+            //);
         }
     }
 }
