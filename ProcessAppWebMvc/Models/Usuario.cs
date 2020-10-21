b07df04bc885ad19c14bafb6efcffe09ddcc51fe
Ruben Alamos <rualamos@gmail.com>
2020-10-21 17:54:33 -0300

    Revert "Se agregan las tablas y sp de el nuevo Script de bd y se deja la conexion desde entitymodel"
    This reverts commit 188d6440d812f73a4ec67111cb2d777ac1059fea.

--- ProcessAppWebMvc/Models/Usuario.cs
+++ ProcessAppWebMvc/Models/Usuario.cs
@@ -8,9 +8,15 @@ namespace ProcessAppWebMvc.Models
     public class Usuario
     {
         public string Nombre { get; set; }
-    
+        public string apellido { get; set; }
+        public string rut { get; set; }
+        public string email { get; set; }
+        public int telefono { get; set; }
+        public string usuario { get; set; }
         public string pass { get; set; }
-    
+        public Rol rol { get; set; }
+        public Unidad unidad { get; set; }
+
 
 
 
--- ProcessAppWebMvc/ProcesService/UsuariosService.cs
+++ ProcessAppWebMvc/ProcesService/UsuariosService.cs
@@ -16,11 +16,11 @@ namespace ProcessAppWebMvc.ProcesService
             return new Usuario()
             {
                 Nombre = "cristopher",
-                //apellido = "Martínez",
-                //rut = "17638455-7",
-                //email = "edad21@gmail.com",
-                //telefono = 445454545,
-                //usuario = "Topher",
+                apellido = "Martínez",
+                rut = "17638455-7",
+                email = "edad21@gmail.com",
+                telefono = 445454545,
+                usuario = "Topher",
                 pass = "12345"
             };
 
@@ -30,41 +30,41 @@ namespace ProcessAppWebMvc.ProcesService
             var usu1 = new Usuario()
             {
                 Nombre = "cristopher",
-                //apellido = "Martínez",
-                //rut = "17638455-7",
-                //email = "edad21@gmail.com",
-                //telefono = 445454545,
-                //usuario = "Topher",
+                apellido = "Martínez",
+                rut = "17638455-7",
+                email = "edad21@gmail.com",
+                telefono = 445454545,
+                usuario = "Topher",
                 pass = "12345"
             };
             var usu2 = new Usuario()
             {
                 Nombre = "Ruben",
-                //apellido = "Alamos",
-                //rut = "17638455-7",
-                //email = "edad21@gmail.com",
-                //telefono = 445454545,
-                //usuario = "Topher",
+                apellido = "Alamos",
+                rut = "17638455-7",
+                email = "edad21@gmail.com",
+                telefono = 445454545,
+                usuario = "Topher",
                 pass = "12345"
             };
             var usu3 = new Usuario()
             {
                 Nombre = "Alex",
-                //apellido = "bd",
-                //rut = "17638455-7",
-                //email = "edad21@gmail.com",
-                //telefono = 445454545,
-                //usuario = "Topher",
+                apellido = "bd",
+                rut = "17638455-7",
+                email = "edad21@gmail.com",
+                telefono = 445454545,
+                usuario = "Topher",
                 pass = "12345"
             };
             var usu4 = new Usuario()
             {
                 Nombre = "ignacio",
-                //apellido = "jp",
-                //rut = "17638455-7",
-                //email = "edad21@gmail.com",
-                //telefono = 445454545,
-                //usuario = "Topher",
+                apellido = "jp",
+                rut = "17638455-7",
+                email = "edad21@gmail.com",
+                telefono = 445454545,
+                usuario = "Topher",
                 pass = "12345"
             };
 
