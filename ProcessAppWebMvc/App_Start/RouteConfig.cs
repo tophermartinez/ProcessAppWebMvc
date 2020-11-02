57b76565d8ba9d5aa7fc9497889a8d7d4b4618fc
Ignacio Caceres <ignaciocaceresp@gmail.com>
2020-11-01 23:30:20 -0300

    Fix bussines

--- ProcessAppWebMvc/App_Start/RouteConfig.cs
+++ ProcessAppWebMvc/App_Start/RouteConfig.cs
@@ -16,9 +16,9 @@ namespace ProcessAppWebMvc
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-              
+
                 defaults: new { controller = "Usuario2", action = "Read", id = UrlParameter.Optional }
-              
+
             );
 
             //routes.MapRoute(
